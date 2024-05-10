f3d89b21ca395f089f79f22270eaba1d43bd24a8
lucaswsanchez <42994495lucas@gmail.com>
2024-05-10 16:22:51 -0300

    commit

--- notes-frontend/src/utils/apiService.js
+++ notes-frontend/src/utils/apiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:4000";
+const API_BASE_URL = "https://notes-app-fullstack-p9df.onrender.com";
 
 const apiService = {
   getAllNotes: async () => {
