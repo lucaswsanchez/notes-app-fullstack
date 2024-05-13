e2366eeaa413dd7637aa13a796308626fa7733bc
lucaswsanchez <42994495lucas@gmail.com>
2024-05-13 10:13:19 -0300

    commit

--- notes-backend/src/app.module.ts
+++ notes-backend/src/app.module.ts
@@ -13,9 +13,10 @@ import { AppService } from './app.service';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
